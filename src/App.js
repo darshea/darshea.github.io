196b7d8ac4bd9da9034f2730d39f8ae10ca34c66
darshea <danbik@cs.sfu.ca>
2023-08-10 14:48:32 -0700

    MODIFIED: loading + route issues

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
       <div className='nav'>
         <Link to="/">
-          <ItemButton text="Danbi Kim" to="/"/>
+          <ItemButton text="Danbi Kim" to='/'/>
         </Link>
       </div>
       <div className='sndnav'>
@@ -19,30 +19,32 @@ function App() {
       <div className="container">
         <div>
           <a href={resume} target="_blank" rel="noreferrer">
-            <ItemButton text="Resume" to="/resume"/> 
+            <ItemButton text="Resume" to='/resume'/> 
           </a>
-          <Link to="/biography">
-            <ItemButton text="Biography" to="/biography" />
+          <Link to='/biography'>
+            <ItemButton text="Biography" to='/biography' />
           </Link>
-          <ItemButton text="GitHub" to="https://github.com/darshea" />
+          <a href="https://github.com/darshea" target="_blank" rel="noreferrer">
+            <ItemButton text="GitHub" to="https://github.com/darshea" />
+          </a>
           <ItemButton text="LinkedIn" to="https://www.linkedin.com/in/danbi-kim-b97833191" />
-          <Link to="/contactme">
-            <ItemButton text="Contact Me" to="/contactme" />
+          <Link to='/contactme'>
+            <ItemButton text="Contact Me" to='/contactme' />
           </Link>
         </div>
         <div>
-          <Link to="/toggle_intro">
-            <ItemButton text="Toggle" to="/toggle_intro"/>
+          <Link to='/toggle_intro'>
+            <ItemButton text="Toggle" to='/toggle_intro'/>
           </Link>
-          <Link to="/rubberduck_intro">
-            <ItemButton text="Rubber Duck" to="/rubberduck_intro" />
+          <Link to='/rubberduck_intro'>
+            <ItemButton text="Rubber Duck" to='/rubberduck_intro' />
           </Link>
           <Link to="/">
-            <ItemButton text="Portfolio" to="https://github.com/darshea/darshea.github.io" />
+            <ItemButton text="Portfolio" to='https://github.com/darshea/darshea.github.io' />
           </Link>
         </div>
         <div>
-          <ItemButton text="Storybook" to="https://github.com/darshea/CMPT-276-Group-13" />
+          <ItemButton text="Storybook" to='https://github.com/darshea/CMPT-276-Group-13' />
           <Link to="/minutebook_intro">
             <ItemButton text="Minutebook Generator" to="/construction" />
           </Link>
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/darshea/darshea.github.io.git">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="toggle_intro" element={<ToggleIntro />} />
